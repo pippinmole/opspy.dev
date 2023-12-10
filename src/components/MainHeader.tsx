e333147abb29dc795014ca54e1068ce3aebbbe45
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-12-10 21:30:41 +0000

    fix: header border color

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "@/components/theme-toggle";
 
 export default async function MainHeader() {
   return (
-    <header className="sticky top-0 z-50 flex justify-center border-b border-gray-200 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 flex justify-center border-b backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex items-center justify-between w-full h-16 max-w-7xl px-4 mx-auto sm:px-6">
         <MainNav />
 
