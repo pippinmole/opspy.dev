080b69e14fdf2ebaf52ed8a8a3a737f37f6bc5ff
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2023-12-21 17:46:05 +0000

    chore: added protection to new job form to make sure user owns a company

--- src/app/jobs/new/page.tsx
+++ src/app/jobs/new/page.tsx
@@ -1,6 +1,16 @@
 import CreateJobForm from "@/components/jobs/create-job-form";
+import { getUserWithCompanyById } from "@/services/userService";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
 export default async function NewJobPage() {
+  const session = await auth();
+  if (!session) return redirect("/");
+
+  const user = await getUserWithCompanyById(session.user.id);
+  if (!user) return;
+  if (!user.company) return redirect("/");
+
   return (
     <main className="flex flex-col max-w-3xl m-auto p-14">
       <CreateJobForm />
