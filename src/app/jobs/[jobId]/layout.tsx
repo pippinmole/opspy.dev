c797a203895397f1e0507f79a6f6d3cd7bad508f
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2023-12-24 15:18:48 +0000

    fix: padding issue on smaller screens for /jobs/[jobId]

--- src/app/jobs/[jobId]/layout.tsx
+++ src/app/jobs/[jobId]/layout.tsx
@@ -30,7 +30,7 @@ export default async function JobsLayout(props: JobsLayoutProps) {
   };
 
   return (
-    <main className="flex flex-col">
+    <main className="flex-1 space-y-4 p-8 pt-6">
       <h1 className="text-2xl font-semibold pb-6">Jobs ({jobs.length})</h1>
 
       <div className={"flex columns-2 gap-2 max-h-[70vh]"}>
