bf09571ea24735ec89cdd4785e4f6c48c161544d
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2024-01-13 16:56:01 +0000

    fix: Warning: Extra attributes from the server: class,styles when using theme-provider.tsx

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,24 @@
+// "use client";
+//
+// import { ThemeProvider as NextThemesProvider } from "next-themes";
+// import { type ThemeProviderProps } from "next-themes/dist/types";
+//
+// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+// }
+// Context: https://github.com/shadcn-ui/ui/issues/1906#issuecomment-1827521490
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import * as React from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => setMounted(true), []);
+
+  return (
+    mounted && <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  );
 }
