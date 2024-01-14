788641f03b654a02f73d19ad588c93f8ee540b04
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2024-01-14 02:41:12 +0000

    feat: added job-filter.tsx functionality

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,11 +1,26 @@
 import { getRandomJobPost } from "@/services/JobService";
 import { redirect } from "next/navigation";
+import serialize from "query-string";
 
 export const metadata = {
   title: "Jobs",
 };
 
-export default async function JobsPage() {
+type JobPageParams = {
+  searchParams: {
+    [key: string]: string | string[];
+  };
+};
+
+export default async function JobsPage({ searchParams }: JobPageParams) {
   const job = await getRandomJobPost();
-  return redirect(`/jobs/${job.id}`);
+
+  // Serialize the searchParams object into a query string
+  const p  = new URLSearchParams();
+  for(const [key, value] of Object.entries(searchParams)) {
+    p.append(key, value.toString());
+  }
+
+  // Append the query string to the redirect URL
+  return redirect(`/jobs/${job.id}?${p.toString()}`);
 }
--- src/components/home/job-filter.tsx
+++ src/components/home/job-filter.tsx
@@ -1,14 +1,30 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function JobFilter() {
+  const router = useRouter();
+
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    router.push(`/jobs?keywords=${search}`);
+  };
+
   return (
     <div className="w-full max-w-sm space-y-2">
-      <form className="flex space-x-2">
+      <form className="flex space-x-2" onSubmit={handleSubmit}>
         <Input
           className="max-w-lg flex-1"
           placeholder="Enter job title or keywords"
           type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <Button type="submit">Search</Button>
       </form>
