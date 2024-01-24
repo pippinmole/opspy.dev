03da1779c81f3e1b219e7e35fb300c4a5343971a
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2024-01-24 23:35:43 +0000

    style: improve heading text on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
         {/*<Announcement />*/}
 
         <PageHeaderHeading>
-          Job Hunting,{" "}
+          Jobs,{" "}
           <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
             Simplified.
           </h1>
