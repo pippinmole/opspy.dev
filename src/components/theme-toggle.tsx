0692979388ff46f43f75e770f04451677285b281
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2024-02-02 20:12:14 +0000

    style: fixed button aligning

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -35,14 +35,14 @@ export function ThemeToggle({ ...props }: ThemeToggleProps) {
       variant="outline"
       size="icon"
       onClick={toggleTheme}
-      className={cn("h-8 w-8", props.className)}
+      className={cn("", props.className)}
     >
       {theme === "system" && <ComputerIcon className="h-[1.2rem] w-[1.2rem]" />}
       {theme === "light" && (
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       )}
       {theme === "dark" && (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-0 dark:scale-100" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
