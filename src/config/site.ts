c77117068907fc98213d673a1f8e7c239617c123
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2024-02-02 16:12:18 +0000

    feat: added og.png

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,11 @@
 export const siteConfig = {
-  name: "opportunify",
+  name: "opspy.dev",
   description:
-    "With opportunify, you can find the best opportunities for you from our wide array of opportunities. Try today for free!",
+    "With opspy, you can find the best opportunities for you from our wide array of opportunities. Try today for free!",
   url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-  ogImage: "https://tx.shadcn.com/og.jpg",
+  // Ensure the base URL does not have a trailing slash before appending /og.jpg
+  ogImage: new URL(
+    "/og.png",
+    process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  ).toString(),
 };
