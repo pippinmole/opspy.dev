374e27cdb0bdac2d99f932af52996e8e08c19faf
Jonathan Ruffles <icondesk1@gmail.com>
2025-02-13 13:52:02 +0000

    chore: run 'npx @tailwindcss/upgrade'

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function Component() {
         </p>
         <div className="mt-6">
           <Link
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             href="/"
           >
             Go back to homepage
